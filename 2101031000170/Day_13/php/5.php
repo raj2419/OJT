40b4dd477e7b5ee190a945b3691caf854330d43e
raj2419 <2101031000170@silveroakuni.ac.in>
2023-07-04 03:03:51 -0700

    Day-13

---
+++ 2101031000170/Day_13/php/5.php
@@ -0,0 +1,9 @@
+<?php
+    $name = "Mihir Patel";
+    $number = 2101031000156;
+    $department = "B.tech (Information Technology)";
+    
+    echo "Name: " . $name . "<hr>";
+    echo "Number: " . $number . "<hr>";
+    echo "Department: " . $department . "<hr>";
+?>
